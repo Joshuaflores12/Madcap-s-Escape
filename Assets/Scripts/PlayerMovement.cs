bd6939987805685b9db6b4f6fca8402bd1b76c21
Joshuaflores12 <joshuagabriellflores@gmail.com>
2025-02-15 12:06:35 +0800

    disabled player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,30 +3,30 @@ using UnityEngine.InputSystem;
 
 public class Playermovement : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D rb;
-    [SerializeField] public float move_speed = 0;
-    [SerializeField] Vector2 direction = Vector2.zero;
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    //[SerializeField] Rigidbody2D rb;
+    //[SerializeField] public float move_speed = 0;
+    //[SerializeField] Vector2 direction = Vector2.zero;
+    //private void Start()
+    //{
+    //    rb = GetComponent<Rigidbody2D>();
+    //}
 
-    private void Update()
-    {
-        rb.linearVelocity = direction * move_speed;
-    }
+    //private void Update()
+    //{
+    //    rb.linearVelocity = direction * move_speed;
+    //}
 
-    public void Move_Event(InputAction.CallbackContext context) 
-    {
-        if(context.performed) 
-        {
-            Vector2 input = context.ReadValue<Vector2>();
-            direction.x = input.x;
-        }
-        // no input
-        else
-        {
-            direction = Vector2.zero;
-        }
-    }
+    //public void Move_Event(InputAction.CallbackContext context) 
+    //{
+    //    if(context.performed) 
+    //    {
+    //        Vector2 input = context.ReadValue<Vector2>();
+    //        direction.x = input.x;
+    //    }
+    //    // no input
+    //    else
+    //    {
+    //        direction = Vector2.zero;
+    //    }
+    //}
 }
