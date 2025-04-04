6880b9dc6c753bf22ff0009c87d599e398d41d1a
CIITSHSDTP00\CIIT <91291v@gmail.com>
2025-04-04 21:07:53 +0800

    move dorm room to cam script

--- Assets/Scripts/Dorm/DormFungusManager.cs
+++ Assets/Scripts/Dorm/DormFungusManager.cs
@@ -2,15 +2,44 @@ using UnityEngine;
 
 public class DormFungusManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject objectToMove;
+    [SerializeField] private float moveSpeed = 2f;
+
+    [SerializeField] private Animator animFadeout;
+    [SerializeField] private GameObject fadeoutObject;
+
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+            if (hit.collider != null && hit.collider.CompareTag("roomDoor1"))
+            {
+                Debug.Log("entering room 1.");
+                TriggerFadeoutAnimation();
+                MoveRoomDown();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TriggerFadeoutAnimation()
     {
-        
+        fadeoutObject.SetActive(true);
+        animFadeout.SetBool("FadeOut", true);
+    }
+
+    public void FadeoutFalse()
+    {
+        animFadeout.SetBool("FadeOut", false);
+    }
+
+    public void MoveRoomDown()
+    {
+        if (objectToMove != null)
+        {
+            objectToMove.transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        }
     }
 }
