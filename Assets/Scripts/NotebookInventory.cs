c3c06a4fbad89d73127f72eef28dff77a14240f4
tokwalover666 <91291v@gmail.com>
2025-04-06 05:21:26 +0800

    collected items stored in next scenes inventory script

--- Assets/Scripts/NotebookInventory.cs
+++ Assets/Scripts/NotebookInventory.cs
@@ -18,23 +18,87 @@ public class NotebookInventory : MonoBehaviour
 
     private void Awake()
     {
+
         if (Instance == null)
+        {
             Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+
         else
             Destroy(gameObject);
     }
+
     private void Update()
     {
-        if (secondChallenge.isSecondChallengeCompleted == true || IsSceneAllowed())
+        if (SceneManager.GetActiveScene().name == "HallwayLeft" || SceneManager.GetActiveScene().name == "2_CanteenDorm")
         {
-            //Debug.Log("Q");
             if (Input.GetKeyDown(KeyCode.Q))
             {
-                notebook.SetActive(!notebook.activeSelf);
+                bool newState = !notebook.activeSelf;
+                notebook.SetActive(newState);
+
+                foreach (Transform child in notebook.transform)
+                {
+                    child.gameObject.SetActive(newState);
+                }
             }
         }
+
+
+        if (SceneManager.GetActiveScene().name == "1_IsolationChamber")
+        {
+            if (secondChallenge.isSecondChallengeCompleted == true)
+            {
+                if (Input.GetKeyDown(KeyCode.Q))
+                {
+                    bool newState = !notebook.activeSelf;
+                    notebook.SetActive(newState);
+
+                    foreach (Transform child in notebook.transform)
+                    {
+                        child.gameObject.SetActive(newState);
+                    }
+                }
+
+            }
+        }
+
     }
 
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        GameObject notebookObj = GameObject.Find("InventoryJournal");
+        GameObject inventoryUIObj = GameObject.Find("InventoryParent");
+
+        if (notebookObj != null && inventoryUIObj != null)
+        {
+            ReinitializeReferences(notebookObj, inventoryUIObj.transform);
+            UpdateInventoryUI();
+        }
+        else
+        {
+            Debug.LogWarning("NotebookInventory: Could not find notebook or inventory UI in scene " + scene.name);
+        }
+        notebook.SetActive(false);
+    }
+
+    public void ReinitializeReferences(GameObject newNotebook, Transform newInventoryUI)
+    {
+        notebook = newNotebook;
+        inventoryUI = newInventoryUI;
+    }
     public bool IsSceneAllowed()
     {
         string currentScene = SceneManager.GetActiveScene().name;
@@ -67,7 +131,7 @@ public class NotebookInventory : MonoBehaviour
         return UpdateInventoryUI();
     }
 
-    GameObject UpdateInventoryUI()
+    public GameObject UpdateInventoryUI()
     {
         foreach (Transform child in inventoryUI)
         {
